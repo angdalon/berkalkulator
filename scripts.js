e585891bc352a69230b0532a6dc5595fd358ea05
angdalon <88055485+angdalon@users.noreply.github.com>
2021-08-29 05:31:19 +0200

    javascript mostly

--- scripts.js
+++ scripts.js
@@ -20,4 +20,25 @@ let calcNetto = () => {
 let calcNetto = () => {
     $('#nettoValue').text(parseInt(bruttoValue.value)*parseFloat(0.665) + " Ft");
     $('#osszkoltsegValue').text(parseInt(bruttoValue.value)*parseFloat(1.17) + " Ft");
+
+    $('#havibruttoValue').text(parseInt(bruttoValue.value) + " Ft");
+    $('#szjaValue').text(parseInt(bruttoValue.value)*parseFloat(0.15) + " Ft");
+    $('#tbValue').text(parseInt(bruttoValue.value)*parseFloat(0.185) + " Ft");
+    $('#munkavallaloiadokValue').text(parseInt($("#szjaValue").text())+parseInt($("#tbValue").text()) + " Ft");
+    $('#evesadoalapValue').text(parseInt(bruttoValue.value)*12 + " Ft");
+    $('#evesszamitottadoValue').text(parseInt($("#szjaValue").text())*12 + " Ft");
 };
+
+
+
+
+/*
+if ($("#checkHazas").is(':checked')) {
+    console.log("valami")
+}
+
+if($("#checkHazas").checked == true) {
+    console.log("valami");
+}
+*/
+
