fac13209c70fe0301d2fd300501e08dd902dc36a
angdalon <88055485+angdalon@users.noreply.github.com>
2021-08-31 04:58:51 +0200

    mostly styling

--- scripts.js
+++ scripts.js
@@ -18,11 +18,11 @@ let calcNetto = () => {
 
 
 let calcValues = () => {
-    if(document.forms["testForm"]["bruttoValue"].value != "" && document.forms["testForm"]["bruttoValue"].value > 0	) {
+    if(document.forms["bruttoInputForm"]["bruttoValue"].value != "" && document.forms["bruttoInputForm"]["bruttoValue"].value > 0	) {
 
         $('#nettoValue').text(parseInt(parseInt(bruttoValue.value)*parseFloat(0.665)) + " Ft");
         $('#osszkoltsegValue').text(parseInt(parseInt(bruttoValue.value)*parseFloat(1.17)) + " Ft");
-
+        
         $('#havibruttoValue').text(parseInt(bruttoValue.value) + " Ft");
         $('#szjaValue').text(parseInt(parseInt(bruttoValue.value)*parseFloat(0.15)) + " Ft");
         $('#tbValue').text(parseInt(parseInt(bruttoValue.value)*parseFloat(0.185)) + " Ft");
@@ -30,21 +30,67 @@ let calcValues = () => {
         $('#evesadoalapValue').text(parseInt(parseInt(bruttoValue.value)*12) + " Ft");
         $('#evesszamitottadoValue').text(parseInt(parseInt($("#szjaValue").text())*12) + " Ft");
     } else {
-        $("#nettoValue").text("0 Ft");
-        $("#osszkoltsegValue").text("0 Ft");
-
-        $("#havibruttoValue").text("0 Ft");
-        $("#szjaValue").text("0 Ft");
-        $("#tbValue").text("0 Ft");
-        $("#munkavallaloiadokValue").text("0 Ft");
-        $("#evesadoalapValue").text("0 Ft");
-        $("#evesszamitottadoValue").text("0 Ft");
-
-        
-   
+        $(".calculated").text("0 Ft");
+       
 }
 
 };
+
+
+
+/*
+$('#plusOneChild').on('click', () => {
+    if(parseInt($('#numberOfChildren').text() = 0)) {
+        let currentChildNum = parseInt($('#numberOfChildren').text());
+        $('#numberOfChildren').text(currentChildNum + 1);
+    }
+    
+    
+});
+
+$('#minusOneChild').on('click', () => {
+    let currentChildNum = parseInt($('#numberOfChildren').text());
+    $('#numberOfChildren').text(currentChildNum - 1);
+});
+*/
+
+/*
+if($('#numberOfChildren').val() < 10) {
+    $('#plusOneChild').on('click', () => {
+        let currentChildNum = parseInt($('#numberOfChildren').val());
+        $('#numberOfChildren').val(currentChildNum + 1);
+    });
+}
+
+$('#minusOneChild').on('click', () => {
+    let currentChildNum = parseInt($('#numberOfChildren').val());
+    $('#numberOfChildren').val(currentChildNum - 1);
+});
+
+<div class="checkboxFlex">
+                    <img src=images/minus_sign.svg alt="minusOne" id="minusOneChild">
+                    <input id="numberOfChildren" type="number" min="0" value="0" oninput="validity.valid||(value='');" >
+                    <img src=images/plus_sign.svg alt="plusOne" id="plusOneChild">
+                </div>
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
